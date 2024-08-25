faaba49376cf4cc5dde43713453652583d8bc31c
Hassan Ali <122170160+Has5an-Ali@users.noreply.github.com>
2024-08-25 10:32:23 +0500

    Dart Data type added

---
+++ datatype.dart
@@ -0,0 +1,36 @@
+//Data type in Dart
+// 1) Number => i) int ii) Double   2) String 3) Boolean
+
+void main() {
+  // int
+  int score = 5;
+  print(score);
+
+  //Double
+  double percentage = 95.4;
+  print(percentage);
+
+  //second Method
+
+  var Score = 4;
+  print(Score);
+
+  var Percentage = 54.3;
+  print(Percentage);
+
+  //String
+
+  String name = "Hassan";
+  print(name);
+
+  var Name = "Hassan Ali";
+  print(Name);
+
+  //Boolean
+
+  bool Isvalid = true;
+  print(Isvalid);
+
+  var Invalid = false;
+  print(Invalid);
+}
