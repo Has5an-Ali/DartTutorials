8538993b254c676d2beaa3daf6b23c612b80acc6
Hassan Ali <122170160+Has5an-Ali@users.noreply.github.com>
2024-09-13 09:45:30 +0500

    Another Example Added in Object & Class

--- classObject.dart
+++ classObject.dart
@@ -6,6 +6,10 @@ void main() {
   car.CarModel = 24;
   car.Carinfo();
   car.CarModelUpdate('Black Color');
+
+  Area area = Area();
+  area.Width = 7;
+  area.calculateRectangle();
 }
 
 class Car {
@@ -22,6 +26,25 @@ class Car {
   void CarModelUpdate(String UpdatecarModel) {
     CarColor = UpdatecarModel;
 
-    print("Color Update To $UpdatecarModel for Car  $CarName");
+    print("Color Update To $UpdatecarModel ");
+  }
+}
+
+class Area {
+  double? Width;
+  double? Height;
+
+  void calculateRectangle() {
+    try {
+      if (Height == null) {
+        print("Height Not be Null");
+        Height = 5;
+        print("We are given value to Height which is $Height");
+      }
+      double calculaterectangle = Width! * Height!;
+      print("The Area is $calculaterectangle");
+    } catch (e) {
+      print(e);
+    }
   }
 }
